450b33698ef7e4b1e90b4a28cacc3abb13ae1ee6
Tajanee Wells <tajaneew16@gmail.com>
2023-03-02 19:31:14 -0500

    pick-a-number

---
+++ index.js
@@ -0,0 +1,10 @@
+let Choice = prompt('pick a number, any number');
+
+let Choice2 = prompt('one more time')
+  
+
+if (Choice >= Choice2) {
+    alert('the larger number between ' + Choice + ' and ' + Choice2 + ' is ' + Choice);
+} else (Choice2>Choice); {
+    alert('the larger number between ' + Choice + ' and ' + Choice2 + ' is ' + Choice2);
+}
